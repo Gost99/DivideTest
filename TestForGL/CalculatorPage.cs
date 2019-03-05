c7a6fdfc48059ff0e799970b5165640dfef2050c
Михайло Простак <misha271999@gmail.com>
2019-03-05 12:10:25 +0200

    init commit

---
+++ TestForGL/CalculatorPage.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using TestStack.White;
+using System.IO;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems;
+
+namespace TestForGL
+{
+    class CalculatorPage
+    {
+        static Window window;
+        public CalculatorPage()
+        {
+            string applicationDirectory = TestContext.CurrentContext.TestDirectory;
+
+            // Just add your AvalonCalculator path like @"D:\Files\AvalonCalculator"
+            string applicationPath = Path.Combine(applicationDirectory, @"D:\PROJECTS\Global Logic Test Task\AvalonCalculator");
+            Application application = Application.Launch(applicationPath);
+            window = application.GetWindow("WPF Calculator", InitializeOption.NoCache);
+        }
+
+        public Button CloseDialogWindow
+        {
+            get
+            {
+                return (Button)window.Get(SearchCriteria.ByAutomationId("2"));
+            }
+            private set { }
+        }
+
+        public Button Btn0 {
+            get
+            {
+                return (Button)window.Get(SearchCriteria.ByAutomationId("B0"));
+            }
+            private set { }
+        }
+
+        public Button BtnDivide
+        {
+            get
+            {
+                return (Button)window.Get(SearchCriteria.ByAutomationId("BDevide"));
+            }
+            private set { }
+        }
+
+        public Button BtnEqual
+        {
+            get
+            {
+                return (Button)window.Get(SearchCriteria.ByAutomationId("BEqual"));
+            }
+            private set { }
+        }
+
+        public Button[] BtnsToClick(string input)
+        {
+            Button[] result = new Button[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                string autoId = "B" + input[i];
+                result[i] = (Button)window.Get(SearchCriteria.ByAutomationId(autoId));
+            }
+            return result;
+        }
+
+        public TextBox DividingResult
+        {
+            get
+            {
+                return (TextBox)window.Get(SearchCriteria.ByAutomationId("txtHistory"));
+            }
+            private set { }
+        }
+
+    }
+}
---
+++ TestForGL/DivideOperationTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TestForGL.Properties;
+
+namespace TestForGL
+{
+    [TestFixture]
+    public class DivideOperationTest
+    {
+        [Test, TestCaseSource("TestingData")]
+        public void DivideByZero(string test)
+        {
+            var page = new CalculatorPage();
+
+            if(!string.IsNullOrEmpty(test))
+            {
+                var buttonsToClick = page.BtnsToClick(test);
+                foreach (var btn in buttonsToClick)
+                {
+                    btn.Click();
+                }
+
+                //divide by zero
+                page.BtnDivide.Click();
+                page.Btn0.Click();
+                page.BtnEqual.Click();
+
+                page.CloseDialogWindow.Click();
+                var calculationResult = page.DividingResult.Text;
+                Assert.That(IsResultExpected(calculationResult));
+            }
+        }
+        public static List<string> TestingData
+        {
+            get
+            {
+                string[] stringSeparators = new string[] { "\r\n" };
+                string[] fileLines = Resources.TestData.Split(stringSeparators, StringSplitOptions.None);
+
+                return fileLines.ToList();
+            }
+        }
+
+        private bool IsResultExpected(string input)
+        {
+            if (input.EndsWith("Error\n"))
+                return true;
+            else
+                return false;
+        }
+    }
+}
